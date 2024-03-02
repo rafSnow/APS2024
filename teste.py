3920f18f2ba290f05a336cf2402da0bb470a8f6a
Rafael <rafael.neves.souzaa@gmail.com>
2024-03-02 11:03:36 -0300

    quem é macaco

--- teste.py
+++ teste.py
@@ -1,4 +1,10 @@
 print('Hello, world')
+macaco = 1
+if macaco == 1:
+    
+    print('lucas é um macaco')
+else:
+    print('elias é um macaco')
 lista = [1, 2, 3, 4, 5]
 for numero in lista:
     print(numero)    
