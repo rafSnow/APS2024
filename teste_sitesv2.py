4b6d2a511f0b713770d869d082774967a4d81ecc
Lucas Neves de Souza Santos <lucas230major@gmail.com>
2024-03-11 21:04:45 -0300

    Ampliação de pesquisa das palavras.

--- teste_sitesv2.py
+++ teste_sitesv2.py
@@ -1,16 +1,16 @@
-import feedparser  # Importa o módulo para análise de feeds RSS
-import requests  # Importa o módulo para fazer solicitações HTTP
-import matplotlib.pyplot as plt  # Importa o módulo para plotar gráficos
-from bs4 import BeautifulSoup  # Importa a classe BeautifulSoup para analisar HTML
-import nltk  # Importa o módulo nltk para processamento de linguagem natural
-from nltk.corpus import stopwords  # Importa a lista de stopwords da nltk
-from nltk.tokenize import word_tokenize  # Importa o tokenizador de palavras da nltk
-nltk.download('stopwords')  # Faz o download das stopwords da nltk
+import feedparser  # Biblioteca para analisar feeds RSS
+import requests  # Biblioteca para fazer solicitações HTTP
+import matplotlib.pyplot as plt  # Biblioteca para criar visualizações de dados
+from bs4 import BeautifulSoup  # Biblioteca para analisar conteúdo HTML
+import nltk  # Biblioteca para processamento de linguagem natural
+from nltk.corpus import stopwords  # Lista de stopwords em português
+from nltk.tokenize import word_tokenize  # Função para tokenizar texto
+nltk.download('stopwords')  # Baixa as stopwords em português, se ainda não tiver sido feito
 
 # Função para obter o conteúdo HTML de um link
 def get_html_content(url):
     try:
-        response = requests.get(url)  # Faz uma solicitação HTTP para obter o conteúdo da página
+        response = requests.get(url)  # Faz uma solicitação HTTP para obter o conteúdo HTML da URL
         response.raise_for_status()  # Lança uma exceção se a solicitação falhar
         return response.text  # Retorna o conteúdo HTML da página
     except requests.RequestException as e:
@@ -20,20 +20,20 @@ def get_html_content(url):
 # Função para coletar todas as palavras de um texto
 def collect_words(text):
     words = word_tokenize(text)  # Tokeniza o texto em palavras
-    words = [word.lower() for word in words if word.isalnum()]  # Remove pontuações e converte para minúsculas
+    words = [word.lower() for word in words if word.isalnum()]  # Converte as palavras para minúsculas e remove pontuações
     return words  # Retorna a lista de palavras
 
 # Função para imprimir as principais notícias de um feed RSS
-def print_top_news_rss(site_name, url, max_news=4):
+def print_top_news_rss(site_name, url, max_news=10):
     try:
         feed = feedparser.parse(url)  # Analisa o feed RSS
         print(f"Principais notícias de {site_name}:")
-        print("="*50)
+        print("=" * 50)
         for i, entry in enumerate(feed.entries[:max_news]):
-            print(f"Notícia {i+1}:")
+            print(f"Notícia {i + 1}:")
             print(entry.title)  # Imprime o título da notícia
             print(entry.link)  # Imprime o link da notícia
-            print("-"*50)
+            print("-" * 50)
     except Exception as e:
         print(f"Erro ao processar o feed RSS {url}: {e}")
 
@@ -46,7 +46,30 @@ rss_feeds = {
 }
 
 # Lista de palavras relacionadas ao meio ambiente que queremos buscar
-environment_related_words = ["queimadas", "enchente", "poluição", "desmatamento", "biodiversidade", "sustentabilidade"]
+environment_related_words = [
+    "queimadas", "enchente", "poluição", "desmatamento", "biodiversidade", "sustentabilidade",
+    "reciclagem", "desflorestamento", "aquecimento global", "preservação", "conservação",
+    "impacto ambiental", "energia renovável", "carbono", "plástico", "floresta", "água",
+    "ar", "fauna", "flora", "clima", "contaminação", "resíduos", "ecossistema",
+    "despejo ilegal de resíduos", "extração ilegal de madeira", "caça furtiva", "pesca predatória",
+    "tráfico de animais silvestres", "despejo de produtos químicos tóxicos", "vazamento de petróleo",
+    "incêndios criminosos", "corrupção ambiental", "desvio de recursos naturais",
+    "exploração ilegal de recursos naturais", "contrabando de espécies protegidas", "urbanização descontrolada",
+    "construções irregulares em áreas de preservação", "agronegócio predatório", "mineração ilegal",
+    "pescaria ilegal em áreas protegidas", "envenenamento de rios e mananciais", "contaminação de alimentos",
+    "desmatamento para fins comerciais", "uso indiscriminado de agrotóxicos", "sobreexplotação de recursos hídricos",
+    "evasão de fiscalização ambiental", "suborno para autorizações ambientais", "impacto ambiental de indústrias poluidoras",
+    "destruição de habitats naturais", "assoreamento de rios e lagos", "atropelamento de fauna silvestre",
+    "derramamento de lixo em áreas protegidas", "exploração sexual de recursos naturais",
+    "pesquisa científica ilegal em fauna e flora", "furtos de patrimônio natural",
+    "introdução de espécies exóticas invasoras", "grilagem de terras", "contrabando de produtos florestais",
+    "roubo de madeira", "extração ilegal de areia e cascalho", "modificação genética de espécies",
+    "descarte irregular de produtos eletrônicos", "produção clandestina de carvão vegetal",
+    "pesca com explosivos ou veneno",
+    "desmatamento", "queimada", "incêndio", "inundações", "poluente", "resíduo", "erosão", "desertificação",
+    "contaminante", "contaminação", "degradação", "seca", "salinização", "desertificação", "assoreamento",
+    "impacto", "alteração", "degradante", "emissão"
+]
 
 # Coleta todas as palavras das notícias
 all_words = []
@@ -60,20 +83,20 @@ for site, rss_feed in rss_feeds.items():
             words = collect_words(text)  # Coleta as palavras do texto
             all_words.extend(words)  # Adiciona as palavras à lista de todas as palavras
 
-# Remover stopwords (palavras comuns que não contribuem significativamente para o significado)
+# Remove stopwords (palavras comuns que não contribuem significativamente para o significado)
 stop_words = set(stopwords.words('portuguese'))  # Obtém as stopwords em português
 filtered_words = [word for word in all_words if word not in stop_words]  # Remove as stopwords das palavras coletadas
 
-# Identificar e contar as palavras relacionadas ao meio ambiente
+# Identifica e conta as palavras relacionadas ao meio ambiente
 environment_words = [word for word in filtered_words if word in environment_related_words]
 word_freq = nltk.FreqDist(environment_words)
 
-# Obter as palavras mais comuns e suas frequências
-top_words = word_freq.most_common(20)  # Obtém as 20 palavras mais comuns e suas frequências
+# Obtém as palavras mais comuns e suas frequências
+top_words = word_freq.most_common(40)
 top_words, frequencies = zip(*top_words)  # Separa as palavras e frequências em duas listas
 
-# Plotar o gráfico de barras das palavras mais comuns relacionadas ao meio ambiente
-plt.figure(figsize=(10, 6))  # Define o tamanho da figura do gráfico
+# Plota o gráfico de barras das palavras mais comuns relacionadas ao meio ambiente
+plt.figure(figsize=(20, 6))  # Define o tamanho da figura do gráfico
 plt.bar(top_words, frequencies, color='green')  # Plota o gráfico de barras com as palavras e frequências
 plt.xlabel('Palavras relacionadas ao Meio Ambiente')  # Adiciona um rótulo ao eixo x
 plt.ylabel('Frequência')  # Adiciona um rótulo ao eixo y
@@ -81,8 +104,3 @@ plt.title('Frequência das palavras relacionadas ao Meio Ambiente nas notícias'
 plt.xticks(rotation=45, ha='right')  # Rotaciona os rótulos do eixo x para facilitar a leitura
 plt.tight_layout()  # Ajusta o layout do gráfico para evitar sobreposições
 plt.show()  # Exibe o gráfico de barras
-
-# Imprimir as principais notícias de cada feed RSS
-for site, rss_feed in rss_feeds.items():
-    print_top_news_rss(site, rss_feed)
-    print("\n")  # Adiciona uma linha em branco entre os resultados
